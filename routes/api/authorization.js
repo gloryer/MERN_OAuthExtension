3e50a9886fa6c11a733f1c8e1406031317739e87
Shuai Li <shuai_lee@outlook.com>
2019-07-19 15:48:09 -0600

    1. debugging access evaluation 2. Adding combined policy evaluation strategy. 3. Adding token generation functionality

--- routes/api/authorization.js
+++ routes/api/authorization.js
@@ -2,13 +2,32 @@ const express =require('express');
 const router =express.Router();
 const RequestEvaluation = require('../../Middleware/RequestValidationandPolicyCheck');
 const Policy =require('../../models/Policy');
+const jwt =require('jsonwebtoken');
 
 
 //route GET api/users
-router.post('/',RequestEvaluation,(req,res)=>{
-    Policy.find()
-        .then(list => res.json(list))
-});
+router.post('/',RequestEvaluation,(req,res)=> {
 
+})
+
+   /* jwt.sign(
+        {id:user.id,
+            expireIn:"1 day",
+            subject:user.name,
+            audience: "http://localhost:5000/petientResource",
+            issuer: "http://localhost:5000/token",
+            structured_scope:{
+                permission: "read",
+                label:"Observation",
+            }},
+        config.get('jwtSecret'),
+        (err,token)=>{
+            if (err) throw err;
+            console.log(token, user);
+            res.json({token, user})
+        }
+    )});
+
+*/
 
 module.exports=router;
